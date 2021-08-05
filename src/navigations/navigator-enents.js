57e2773674566b4bbdde7167c3321a7b9fbf9c9a
Cherish0207 <cheshire_1207@foxmail.com>
2021-08-05 17:35:15 +0800

    拦截路径切换操作-history api

--- src/navigations/navigator-enents.js
+++ src/navigations/navigator-enents.js
@@ -5,7 +5,7 @@ const capturedEventListeners = {
   // 存储hashchang和popstate注册的方法
   // 后续挂载的事件先暂存起来
   hashchange: [],
-  popstate: [],
+  popstate: [], // 当应用切换完成后可以调用
 };
 function urlReroute() {
   reroute([], arguments); // 会根据路径重新加載不同的应用
@@ -39,3 +39,39 @@ window.removeEventListener = function (eventName, listenerFn) {
   }
   return originalRemoveEventListener.apply(this, arguments);
 };
+
+// 如果是hash路由,hash变化时可以切换
+// 如果是浏览器路由,浏览器路由是h5 api,切换时不会触发popstate
+function patchedUpdateState(updateState, methodName) {
+  return function () {
+    const urlBefore = window.location.href;
+    const result = updateState.apply(this, arguments);
+    const urlAfter = window.location.href;
+    if (urlBefore !== urlAfter) {
+      // 重新加载应用传入事件源
+      urlReroute(new PopStateEvent("popstate")); // 构建一个popstate事件源
+    }
+    return result;
+  };
+}
+// 重写pushState 和 repalceState方法
+window.history.pushState = patchedUpdateState(
+  window.history.pushState,
+  "pushState"
+);
+window.history.replaceState = patchedUpdateState(
+  window.history.replaceState,
+  "replaceState"
+);
+
+// 在子应用加载完毕后调用此方法，执行拦截的逻辑（保证子应用加载完后执行）
+export function callCapturedEventListeners(eventArguments) {
+  if (eventArguments) {
+    const eventType = eventArguments[0].type;
+    if (routingEventsListeningTo.indexOf(eventType) >= 0) {
+      capturedEventListeners[eventType].forEach((listener) => {
+        listener.apply(this, eventArguments);
+      });
+    }
+  }
+}
